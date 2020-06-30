4c017525bb372412d80454355a8207c215bb3c7b
guy <guy@shitchell.com>
2020-06-29 23:42:37 -0400

    added closing message

--- broken.sh
+++ broken.sh
@@ -3,3 +3,4 @@
 echo 'Challenge 2'
 read -p 'What is your name: ' $username
 echo "Thanks for playing, $username! Good luck!"
+echo "Make sure to add the commit ID that broke this program to 'broken.txt' once you find it!"
