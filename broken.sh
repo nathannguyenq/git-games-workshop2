fb81aa8bb621ab768f805ecb8845fa1b50ad1f27
guy <guy@shitchell.com>
2020-06-29 23:42:57 -0400

    minor update

--- broken.sh
+++ broken.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo 'Challenge 2'
+echo
 read -p 'What is your name: ' $username
 echo "Thanks for playing, $username! Good luck!"
 echo "Make sure to add the commit ID that broke this program to 'broken.txt' once you find it!"
