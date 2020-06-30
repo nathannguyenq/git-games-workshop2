11e6ad84a6541e54369e7e0d536dfdb6a13fd582
guy <guy@shitchell.com>
2020-06-29 23:44:31 -0400

    fixed stuff

--- broken.sh
+++ broken.sh
@@ -4,4 +4,4 @@ echo 'Challenge 2'
 echo
 read -p 'What is your name: ' $username
 echo "Thanks for playing, $username! Good luck!"
-echo "Make sure to add the commit ID that broke this program to 'broken.txt' once you find it!"
+echo "Make sure to add the commit hash that broke this program to 'broken.txt' once you find it!"
