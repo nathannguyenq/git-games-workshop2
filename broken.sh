edc1092cffb034b2c33c34a4fe4c5ab2c0ee3752
Shaun <shauni3dud3@gmail.com>
2020-10-03 01:22:32 -0400

    replaced broken.sh with Broken.java

--- broken.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo 'Challenge 2'
-echo
-read -p 'What is your name: ' $username
-echo "Thanks for playing, $username! Good luck!"
-echo "Make sure to add the commit hash that broke this program to 'broken.txt' once you find it!"
